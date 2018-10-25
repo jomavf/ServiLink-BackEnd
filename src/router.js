77dae5f4ecbba9aa99605406b054640091ce052b
Jose Veliz <jose.veliz.francia@gmail.com>
2018-10-24 21:32:05 -0500

    updated home page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import service from './routes/services'
 import auth from './routes/auth'
+import home from './routes/home'
 
 export default app => {
     app.use('/auth', auth)
     app.use('/service', service)
+    app.use('/',home)
 }
