157b142187fb25485bc039e48700f94b715e4367
Jose Veliz <jose.veliz.francia@gmail.com>
2018-10-24 21:11:33 -0500

    database connection string updated

--- src/database.js
+++ src/database.js
@@ -5,7 +5,8 @@ const SETTINGS = config()
 
 export default () => {
     mongoose.connect(
-        `${SETTINGS.parsed.DB_PROTOCOL}://${SETTINGS.parsed.DB_URL}:${SETTINGS.parsed.DB_PORT}/${SETTINGS.parsed.DB_NAME}`,
+        // `${SETTINGS.parsed.DB_PROTOCOL}://${SETTINGS.parsed.DB_URL}:${SETTINGS.parsed.DB_PORT}/${SETTINGS.parsed.DB_NAME}`,
+        "mongodb+srv://zetagh:zetaghpwd@cluster0-zjrjx.mongodb.net/servilink?retryWrites=true",
         { useNewUrlParser: true }
     ).then(() => { console.log('Successful connection to MongoDB') })
         .catch(err => console.log(`The error is: ${err}`))
