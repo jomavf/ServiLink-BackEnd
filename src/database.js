5172d3aa49bef2ca3e90ca14d135c4d955a2c8fe
Jose Veliz <jose.veliz.francia@gmail.com>
2018-11-16 22:28:36 -0500

    push

--- src/database.js
+++ src/database.js
@@ -6,7 +6,8 @@ import mongoose from 'mongoose'
 export default () => {
     mongoose.connect(
         // `${SETTINGS.parsed.DB_PROTOCOL}://${SETTINGS.parsed.DB_URL}:${SETTINGS.parsed.DB_PORT}/${SETTINGS.parsed.DB_NAME}`,
-        "mongodb+srv://zetagh:zetaghpwd@cluster0-zjrjx.mongodb.net/test?retryWrites=true",
+        // "mongodb+srv://zetagh:zetaghpwd@cluster0-zjrjx.mongodb.net/test?retryWrites=true",
+        "mongodb://zetagh:zetagh123@ds161856.mlab.com:61856/heroku_x2tz5x1j",
         { useNewUrlParser: true }
     ).then(() => { console.log('Successful connection to datbase MongoDB') })
         .catch(err => console.log(`Error while connecting to database: ${err}`))
