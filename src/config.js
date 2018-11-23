37ebfce089f2cc3943e268b61fa8590e6806cd69
Jose Veliz <jose.veliz.francia@gmail.com>
2018-11-23 15:49:59 -0500

    important

--- src/config.js
+++ src/config.js
@@ -1,22 +1,23 @@
 import { config } from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const SETTINGS = config()
 
 export default app => {
     app.disable('x-powered-by')
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-
+    
     app.set('port', process.env.PORT || 5000)
     app.set('config', SETTINGS.parsed)
     app.locals.env = app.get('env')
     app.locals.config = app.get('config')
-
+    
     if (process.env.ENV === 'dev') {
         app.use(morgan("dev"))
     }
     app.use(cors())
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
 }
